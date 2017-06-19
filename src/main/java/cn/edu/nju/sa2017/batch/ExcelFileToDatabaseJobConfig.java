265640d6dd59365fd4e4e92f0adae190a322aedd
njucjc <njucjc@gmail.com>
2017-06-19 11:03:02 +0800

    refactor

--- src/main/java/cn/edu/nju/sa2017/batch/ExcelFileToDatabaseJobConfig.java
+++ src/main/java/cn/edu/nju/sa2017/batch/ExcelFileToDatabaseJobConfig.java
@@ -47,36 +47,7 @@ public class ExcelFileToDatabaseJobConfig {
 
     @Autowired
     StepBuilderFactory stepBuilderFactory;
-
-
-/*    public static class ExcelFileToDatabaseItemProcessor implements ItemProcessor<String[], Student> {
-
-        private Logger logger = LoggerFactory.getLogger(ExcelFileToDatabaseItemProcessor.class);
-
-        @Override
-        public Student process(String[] tokens) throws Exception {
-            Student student = new Student(
-                    "141220008",
-                    "Jim",
-                    "CS",
-                    "正选",
-                    10.0,
-                    20.0,
-                    70.0
-            );
-            student.setId(tokens[0]);
-            student.setName(tokens[1]);
-            student.setMajor(tokens[3]);
-            student.setType(tokens[2]);
-            student.setUsual(Double.parseDouble(tokens[4]));
-            student.setDesign(Double.parseDouble(tokens[5]));
-            student.setExam(Double.parseDouble(tokens[6]));
-            logger.info("get a student, his id is " + student.getId());
-
-            return student;
-        }
-    }
-*/
+    
     @Component
     public static class ExcelFileToDatabaseJobExecutionListener implements JobExecutionListener {
 
